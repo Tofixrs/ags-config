bda78cf4c46fe6b25c4a84a0b904d1ddb2bb47d9
tofixrs <tofix.rs@proton.me>
2023-09-23 14:54:15 +0200

    fixed workspaces

--- modules/hyprland.js
+++ modules/hyprland.js
@@ -1,27 +1,26 @@
-const {Box, Button, Label, Icon} = ags.Widget;
-const {execAsync, lookUpIcon} = ags.Utils;
-const {Hyprland} = ags.Service;
+const { Box, Button, Label, Icon } = ags.Widget;
+const { execAsync, lookUpIcon } = ags.Utils;
+const { Hyprland } = ags.Service;
 
-export const workspaces = ({indicator, fixed = 5} = {}) => Box({
-	children: Array.from({length: fixed}, (_, i) => i+1).map(i => workspaceBtn(i, indicator)),
+export const workspaces = ({ indicator, fixed = 5 } = {}) => Box({
+	children: Array.from({ length: fixed }, (_, i) => i + 1).map(i => workspaceBtn(i, indicator)),
 	connections: [[Hyprland, box => {
-		const {workspaces} = Hyprland;
+		const { workspaces } = Hyprland;
 		const otherWs = getOtherWorkspaces(workspaces, fixed);
 
 		if (otherWs.length > 0) {
 			const otherWsIDs = otherWs.map(ws => ws.id);
-			box.children = Array.from({length: fixed}, (_, i) => i+1)
+			box.children = Array.from({ length: fixed }, (_, i) => i + 1)
 				.concat(otherWsIDs)
 				.map(i => workspaceBtn(i, indicator));
 		} else if (box.children.length > 5) {
-			box.children = Array.from({length: fixed}, (_, i) => i+1).map(i => workspaceBtn(i, indicator));
+			box.children = Array.from({ length: fixed }, (_, i) => i + 1).map(i => workspaceBtn(i, indicator));
 		}
 	}]]
 });
 
 function getOtherWorkspaces(workspaces, fixed) {
-	const ws = Array.from(workspaces.values());
-	return ws.filter(workspace => workspace.id > fixed)
+	return workspaces.filter(workspace => workspace.id > fixed)
 }
 
 function workspaceBtn(id, indicator) {
@@ -29,8 +28,8 @@ function workspaceBtn(id, indicator) {
 		onClicked: () => execAsync(`hyprctl dispatch workspace ${id}`),
 		child: indicator ? indicator() : Label(`${id}`),
 		connections: [[Hyprland, btn => {
-			const {workspaces, active} = Hyprland;
-			const occupied = workspaces.has(id) && workspaces.get(id).windows > 0;
+			const { active } = Hyprland;
+			const occupied = Hyprland.getWorkspace(i)?.windows > 0;
 			btn.toggleClassName('active', active.workspace.id === id);
 			btn.toggleClassName('occupied', occupied);
 			btn.toggleClassName('empty', !occupied);
