dd0509d650f2d76df62c72f6d1247e29b6ff0509
tofixrs <tofix.rs@proton.me>
2024-11-23 13:54:27 +0100

    fix login dialog picking wrong session

--- src/services/PowerMenu.ts
+++ src/services/PowerMenu.ts
@@ -1,48 +1,52 @@
 import GObject, { register, property } from "astal/gobject";
 import { App } from "astal/gtk3";
 import { execAsync, exec } from "astal";
+import { user } from "@lib/consts";
 
 export const PowerAction = {
-  Shutdown: 0,
-  Sleep: 1,
-  Logout: 2,
-  Reboot: 3,
+	Shutdown: 0,
+	Sleep: 1,
+	Logout: 2,
+	Reboot: 3,
 } as const;
 
 export type PAction = (typeof PowerAction)[keyof typeof PowerAction];
 
 @register()
 class PowerMenu extends GObject.Object {
-  @property(Number)
-  private declare currentAction?: PAction;
-  public confirmAction(action: PAction) {
-    this.currentAction = action;
-    App.get_window("powerMenu")?.close();
-    App.get_window("verification")?.show_all();
-  }
+	@property(Number)
+	private declare currentAction?: PAction;
+	public confirmAction(action: PAction) {
+		this.currentAction = action;
+		App.get_window("powerMenu")?.close();
+		App.get_window("verification")?.show_all();
+	}
 
-  public action() {
-    switch (this.currentAction) {
-      case PowerAction.Shutdown: {
-        execAsync("shutdown 0");
-        break;
-      }
-      case PowerAction.Sleep: {
-        execAsync("systemctl suspend");
-        break;
-      }
-      case PowerAction.Logout: {
-        // Gotta love parsing strings lol
-        const session = exec("loginctl").split("\n")[1].split(/\s+/)[1];
-        execAsync(`loginctl kill-session ${session}`);
-        break;
-      }
-      case PowerAction.Reboot: {
-        execAsync("reboot");
-        break;
-      }
-    }
-  }
+	public action() {
+		switch (this.currentAction) {
+			case PowerAction.Shutdown: {
+				execAsync("shutdown 0");
+				break;
+			}
+			case PowerAction.Sleep: {
+				execAsync("systemctl suspend");
+				break;
+			}
+			case PowerAction.Logout: {
+				const regex = new RegExp(`${user}.*user`);
+				const session = exec("loginctl")
+					.split("\n")
+					.find((v) => regex.test(v))
+					?.split(/\s+/)[1];
+				execAsync(`loginctl kill-session ${session}`);
+				break;
+			}
+			case PowerAction.Reboot: {
+				execAsync("reboot");
+				break;
+			}
+		}
+	}
 }
 
 export const PowerMenuService = new PowerMenu();
