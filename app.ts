d599f72c15058d72daefcfff94e6825a518d58ab
Tofixrs <73693639+Tofixrs@users.noreply.github.com>
2024-12-14 14:28:11 +0100

    feat: multi mon support

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { App } from "astal/gtk3";
+import { App, Gdk, Gtk } from "astal/gtk3";
 import style from "./style/main.scss";
 import Bar from "./src/widget/Bar/Bar";
 import { PowerMenu, Verification } from "src/widget/PowerMenu";
@@ -8,10 +8,28 @@ import { Dashboard } from "src/widget/Dashboard/Dashboard";
 App.start({
 	css: style,
 	main() {
-		App.get_monitors().map(Bar);
-		App.get_monitors().map(NotificationPopups);
+		const multiwindowWidgets = [Bar, NotificationPopups];
+		const monMap = new Map<Gdk.Monitor, Gtk.Widget[]>();
+		App.get_monitors().forEach((mon) => {
+			monMap.set(
+				mon,
+				multiwindowWidgets.map((widget) => widget(mon)),
+			);
+		});
 		PowerMenu();
 		Verification();
 		Dashboard();
+
+		App.connect("monitor-added", (_, mon) => {
+			monMap.set(
+				mon,
+				multiwindowWidgets.map((widget) => widget(mon)),
+			);
+		});
+
+		App.connect("monitor-removed", (_, mon) => {
+			monMap.get(mon)?.forEach((v) => v.destroy());
+			monMap.delete(mon);
+		});
 	},
 });
